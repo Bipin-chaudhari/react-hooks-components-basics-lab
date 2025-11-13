9b2d6a91c3dc3de345596d44302e0fcae916a8d0
+ <+>
2025-11-13 21:50:57 +1100

    imported and added home and about \component

--- src/components/App.js
+++ src/components/App.js
@@ -16,8 +16,14 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+{/* write an <About> component here */ }
+function About() {
+  return (
+    <div id="about">
+      <h1>About</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
@@ -25,6 +31,8 @@ function App() {
       <NavBar />
       {/* add the <Home> component here */}
       {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
